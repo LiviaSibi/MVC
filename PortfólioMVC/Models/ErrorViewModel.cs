c383c2a9ab1d28a06f63ca570f5eed34a6d3571d
LiviaSibi <livia.sibinelli@gmail.com>
2019-11-06 12:27:39 -0200

    first commit

---
+++ PortfólioMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PortfólioMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
