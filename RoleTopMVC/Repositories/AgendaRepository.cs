5acb51b5c249cde136039f91facba1bb9908605d
LiviaSibi <livia.sibinelli@gmail.com>
2019-11-21 12:20:54 -0200

    atualização 21/11

--- RoleTopMVC/Repositories/AgendaRepository.cs
+++ RoleTopMVC/Repositories/AgendaRepository.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using RoleTopMVC.Models;
 
 namespace RoleTopMVC.Repositories
 {
@@ -12,6 +13,15 @@ namespace RoleTopMVC.Repositories
             }
         }
 
-        
+        public bool Inserir (Agenda agenda){
+            var linha = new string[] {PrepararRegistroCSV(agenda)};
+            File.AppendAllLines(PATH, linha);
+            return true;
+        }
+
+        private string PrepararRegistroCSV(Agenda agenda){
+            Cliente cliente = agenda.Cliente;
+            return $"cliente_nome={cliente.Nome};cliente_email={cliente.Email};cliente_telefone={cliente.Telefone};data_evento={agenda.DataDoEvento};hamburguer_preco={hamburguer.Preco};shake_nome={shake.Nome};shake_preco={shake.Preco};data_pedido={pedido.DataDoPedido};preco_total={pedido.PrecoTotal}";
+        }
     }
 }
